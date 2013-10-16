69d9c79832d7a6d1032b8828b5776d8b8a6b39f2
iuri aranda <iuri.aranda@uwhisp.com>
2013-10-16 11:56:45 +0200

    Fix a git_revision

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,5 +8,5 @@
 default[:yasm][:install_method] = :package
 default[:yasm][:prefix] = "/usr/local"
 default[:yasm][:git_repository] = "git://github.com/yasm/yasm.git"
-default[:yasm][:git_revision] = "HEAD"
+default[:yasm][:git_revision] = "607feba6d98bf549513e4eaa21445328f9c2b834"
 default[:yasm][:compile_flags] = []
